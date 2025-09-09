25a5d0a973e44f4e605e64264e44536d63bb017a
Hima Sekhar v <himasekhar9963@gmail.com>
2025-09-09 13:02:57 +0530

    new thing

---
+++ src/test/java/hashmap.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class hashmap {
+    public static void main(String[] args) {
+        HashMap <String,String> map=new HashMap<>();
+
+        map.put("1","Hima");
+        map.put("2","Hima");
+        map.put("3","sai");
+        map.put("4","seenu");
+        System.out.println(map);
+    }
+}
